5e9c2153e8f528d92e968a79b075e7d0c89d3b94
Sudipta Saha Niloy <niloy305968@gmail.com>
2021-07-02 16:51:05 +0600

    Final commit I promise

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,24 @@
 // declaring UI
 let day_button = document.querySelectorAll(".button-day");
+let reset_button = document.querySelector('.reset-btn');
 
 // declaring event listener
 day_button.forEach(element => { element.addEventListener('dblclick', dayDone); });
+// day_button.forEach(element => {
+//     if (element.style.background == '') {
+//         element.addEventListener('click', dayDone);
+//     } else {
+//         element.addEventListener('click', clearDay);
+//     }
+// });
 day_button.forEach(element => { element.addEventListener('click', clearDay); });
 document.addEventListener('DOMContentLoaded', reloadDayDone);
+reset_button.addEventListener('click', resetAll);
+
 
 // function to change color of the button day done
 function dayDone(e) {
-    e.target.style.background = '#1aff1a';
+    e.target.style.background = '#80ff80';
     local_storage.saveDaysToLS(e.target.textContent);
 }
 
@@ -24,7 +34,7 @@ function reloadDayDone(e) {
     for (x in days) {
         for (y in day_button) {
             if (days[x] == day_button[y].textContent) {
-                day_button[y].style.background = '#1aff1a';
+                day_button[y].style.background = '#80ff80';
             }
         }
     }
@@ -34,4 +44,13 @@ function reloadDayDone(e) {
 function clearDay(e) {
     e.target.style.background = '';
     local_storage.dayUnDone(e.target);
+}
+
+// function to clear all the days
+function resetAll(e) {
+    localStorage.clear();
+
+    day_button.forEach(element => {
+        element.style.background = '';
+    });
 }
